457a16f04b828f7deb23b7a34d111fd5ceadc478
Jeferson <alexandermejicanos57@gmail.com>
2022-05-03 23:59:56 -1200

    Default Params y Concatenación

---
+++ src/es6/index.js
@@ -0,0 +1,27 @@
+//Default Params o parámetros predeterminados
+//Antes de ECMASCript 6+
+function newFunction(name, age, country) {
+    var name = name || 'Jeferson'; 
+    var age = age || 18;
+    var country = country || 'GT';
+    console.log(name, age, country);
+}
+
+//Después de ECMAScript 6+
+function newFunction2(name = 'Jeferson', age = 18, country = 'GT') {
+    console.log(name, age, country);
+}
+
+newFunction2();
+newFunction2('Ricardo', 23, 'COL');
+
+//Concatenación: nos permite unir varios elemento 
+//Antes de ECMAScript 6+
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+//Después de ECMAScript 6+
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
