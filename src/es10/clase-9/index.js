8c57c7e6bc7e803459b79e8a140607b67687f7b8
Jeferson <alexandermejicanos57@gmail.com>
2022-06-06 15:57:45 -1200

    ES10 con notas y bugs areglados

--- src/es10/clase-9/index.js
+++ src/es10/clase-9/index.js
@@ -1,11 +1,14 @@
+//Array.flat()
 let array = [1, 2, 3, [1, 2, 3, [1, 2, 3]]];
-
 console.log(array.flat(2));
 
-let array = [1, 2, 3, 4, 5];
 
-console.log(array.flatMap(values => [value, values * 2]));
+//Array.flatMap()
+let array2 = [1, 2, 3, 4, 5];
+console.log(array2.flatMap(value => [value, value * 2]));
 
+
+//.trimStart()
 let helloStart = '         Hello World';
 let helloEnd = 'Hello World        ';
 console.log(helloStart, helloEnd);
@@ -13,6 +16,7 @@ console.log(helloStart.trimStart());
 console.log(helloEnd.trimEnd());
 
 
+//Optional catch biding
 try {
 
 } catch {
@@ -20,9 +24,12 @@ try {
 }
 
 
-let entries = [["name, jeferson"], ["age", 18]];
+//.fromEntries()
+let entries = [["name", "jeferson"], ["age", 18]];
 console.log(Object.fromEntries(entries));
 
+
+//Objeto de tipo símbolo
 let mySymbol = 'My symbol';
 let symbol = Symbol(mySymbol);
 console.log(symbol.description);
